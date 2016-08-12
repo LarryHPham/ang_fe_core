d63961af1823cad932b3ca62c1e8fb202e26b966
victoriale <le.victoriavan@gmail.com>
2016-08-12 10:19:06 -0500

    footer service set up

--- components/footer/footer.component.ts
+++ components/footer/footer.component.ts
@@ -5,13 +5,13 @@ import {MLBGlobalFunctions} from '../../../global/mlb-global-functions';
 import {Link, NavigationData} from '../../../global/global-interface';
 import {GlobalSettings} from "../../../global/global-settings";
 import {DropdownDirectoryComponent} from '../dropdown-directory/dropdown-directory.component';
-
+import {FooterService} from '../../../services/footer.service';
 @Component({
     selector: 'footer-component',
     templateUrl: './app/fe-core/components/footer/footer.component.html',
     directives: [ROUTER_DIRECTIVES, DropdownDirectoryComponent],
     inputs: [],
-    providers: [],
+    providers: [FooterService],
 })
 export class FooterComponent implements OnInit {
     @Input() partner: string;
@@ -25,21 +25,29 @@ export class FooterComponent implements OnInit {
     public _siteFacebookUrl: string = GlobalSettings.getSiteFacebookUrl();
     public _siteGoogleUrl: string = GlobalSettings.getSiteGoogleUrl(this.partner);
     public _sportLeagueFull: string = GlobalSettings.getSportLeagueFull();
-    public _lastUpdated: string = "© 2016 ";
+    public _lastUpdated: string = GlobalSettings.getEstYear();
     teamDirectoryListings: Array<Link> = [];
 
     playerDirectoryListings: Array<Link> = [];
 
     mlbTeamListings: Array<Link> = [];
-
+    constructor(private _service: FooterService){//TODO
+      this._service.getFooterService("nfl", "player")
+      .subscribe(data => {
+        // this.playerDirectoryListings = data;
+      },
+      err => {
+        console.log("Error getting footer data");
+      });
+    }
     loadData(partner: string) {
       var checkPartner = GlobalSettings.getHomeInfo().isPartner;
       if(!partner && !checkPartner) {
           this.pageName = GlobalSettings.getBaseTitle();
-          this._lastUpdated += GlobalSettings.getBaseTitle();
+          this._lastUpdated += " " + GlobalSettings.getBaseTitle();
      } else {
           this.pageName = GlobalSettings.getBasePartnerTitle();
-          this._lastUpdated += GlobalSettings.getBasePartnerTitle();
+          this._lastUpdated += " " + GlobalSettings.getBasePartnerTitle();
       }
     }
 
