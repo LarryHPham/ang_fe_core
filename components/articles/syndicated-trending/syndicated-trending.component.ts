e6d4faa604bf80595dba71fb4532b744bc6f28d3
Navyaeetaram <navya.eetaram@sntmedia.com>
2016-09-04 10:50:33 -0500

    finalSyndicate

--- components/articles/syndicated-trending/syndicated-trending.component.ts
+++ components/articles/syndicated-trending/syndicated-trending.component.ts
@@ -4,6 +4,7 @@ import {ShareLinksComponent} from "../shareLinks/shareLinks.component";
 import {SanitizeHtml} from "../../../pipes/safe.pipe";
 import {ResponsiveWidget} from '../../../components/responsive-widget/responsive-widget.component';
 import {DeepDiveService} from '../../../../services/deep-dive.service';
+import {ShareButtonComponent} from "../../share-button/share-button.component";
 
 declare var moment;
 declare var jQuery: any;
@@ -11,7 +12,7 @@ declare var jQuery: any;
 @Component({
     selector: 'syndicated-trending-component',
     templateUrl: './app/fe-core/components/articles/syndicated-trending/syndicated-trending.component.html',
-    directives: [ShareLinksComponent, ROUTER_DIRECTIVES, ResponsiveWidget],
+    directives: [ShareLinksComponent, ROUTER_DIRECTIVES, ResponsiveWidget,ShareButtonComponent],
     inputs: ['trendingData', 'trendingImages'],
     pipes: [SanitizeHtml],
     providers: [DeepDiveService]
@@ -35,7 +36,9 @@ export class SyndicatedTrendingComponent {
         this._deepdiveservice.getDeepDiveBatchService(numItems, 10, state).subscribe(
           data => {
             this.articleData = this._deepdiveservice.transformTrending(data.data, currentArticleId);
+
             if (this.trendingLength <= 20) {
+                console.log("adrticle data", this.articleData);
             this.trendingLength = this.trendingLength + 10;
             }
           }
@@ -52,4 +55,9 @@ export class SyndicatedTrendingComponent {
           jQuery('#loadingArticles').hide();
         }
       }
+    formatDate(date) {
+         //moment(date, "YYYY-MM-Do").format("MM DD, YYYY at HH:MM A");
+        return moment.unix(date/1000).format("MMMM DD, YYYY at h:mm A") + " EST"
+
+    }
 }
