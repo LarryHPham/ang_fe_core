f51d036586b593ad368710b62bbbbe037ba19261
victoriale <le.victoriavan@gmail.com>
2016-09-23 14:26:06 -0500

    update video stacks

--- modules/deep-dive-blocks/deep-dive-block-1/deep-dive-block-1.module.ts
+++ modules/deep-dive-blocks/deep-dive-block-1/deep-dive-block-1.module.ts
@@ -1,30 +1,35 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { DeepDiveService } from '../../../../services/deep-dive.service';
-// import {SanitizeHtml} from "../../pipes/safe.pipe";
+import { VideoStackData } from "../../../interfaces/deep-dive.data";
+
 declare var moment;
 
 @Component({
   selector: 'deep-dive-block-1',
   templateUrl: './app/fe-core/components/video-stack/video-stack.component.html',
-  // pipes: [SanitizeHtml]
 })
 
 export class DeepDiveBlock1 implements OnInit {
-  videoData: any; //TODO
+  videoDataTop: Array<VideoStackData>;
+  videoDataBatch: Array<VideoStackData>;
   constructor(private _deepDiveData: DeepDiveService){}
 
-  private getDeepDiveVideoBatch(){
-    this._deepDiveData.getDeepDiveVideoBatchService('fbs', 5, 1).subscribe(
-      data => {
-        this.videoData = data.data;
-      },
-      err => {
+  getDeepDiveVideo(){
+      this._deepDiveData.getDeepDiveVideoBatchService('fbs', 5, 1).subscribe(
+        data => {
+          let videoOne = [data.data[0]];
+          let videoBatch = data.data.splice(1,5);
+          this.videoDataTop = videoOne;
+          this.videoDataBatch = videoBatch;
+          // console.log(this.videoDataTop, this.videoDataBatch);
+        },
+        err => {
           console.log("Error getting video batch data");
       });
   }
 
   callModules(){
-    this.getDeepDiveVideoBatch();
+    this.getDeepDiveVideo();
   }
 
   ngOnInit() {
