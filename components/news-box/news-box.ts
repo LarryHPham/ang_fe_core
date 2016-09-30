ba7eeee6ed1e689694ac6edab84035163dc42771
larryhpham <larrypham1991@yahoo.com>
2016-09-30 09:14:23 -0500

    fixing news box style

--- components/news-box/news-box.ts
+++ components/news-box/news-box.ts
@@ -1,4 +1,4 @@
-import {Component, AfterContentChecked, Input, Output, EventEmitter, ElementRef} from '@angular/core';
+import {Component, Input, ElementRef} from '@angular/core';
 import { Larousel } from '../larousel/larousel';
 
 declare var moment:any;
@@ -14,10 +14,44 @@ export class NewsBox{
   @Input() carData: any;
   private displayedItems: any;
 
+  constructor(private _elRef: ElementRef){
+
+  }
 
   displayArray(event){
     if(event != null){
       this.displayedItems = event;
+    }
+  }
+
+  lineClamp(event){
+    let getClass = this._elRef.nativeElement.getElementsByClassName('newsbox-item-container-right-content');
+
+    for(var i = 0; i < getClass.length;i++){
+      let curClass = getClass[i];
+      console.log(curClass);
+      let fontSize = curClass.style['font-size'];
+      console.log(fontSize);
+      var heightLarge = curClass.offsetTop;
+      console.log(1,heightLarge);
+
+      // console.log(curClass.offsetTop, this._elRef.nativeElement.getElementsByClassName('newsbox-item-container-right-fullstory')[0].offsetTop);
+      //
+      // //get vertical distance dynamically between the top of the image and the main horizontal rule.
+      // heightLarge = (Math.abs(curClass.offsetTop - this._elRef.nativeElement.getElementsByClassName('newsbox-item-container-right-fullstory')[0].offsetTop));
+      // console.log(2,heightLarge);
+      // //set max-height for text container.
+      // console.log(curClass.style);
+      // curClass.style['max-height'] = heightLarge + 'px';
+      // //Dynamically parse the number of lines by dividing the max-height of the container by the line height.
+      // var topLarge = curClass,
+      // lineHeight = parseInt(window.getComputedStyle(topLarge).getPropertyValue("line-height"));
+      // var linesLargeTop = Math.floor(heightLarge / lineHeight);
+      // console.log(topLarge);
+      // console.log(linesLargeTop);
+      // //set the line clamp with the line number equation.
+      // topLarge.style['-webkit-line-clamp'] = linesLargeTop;
+
     }
   }
 
