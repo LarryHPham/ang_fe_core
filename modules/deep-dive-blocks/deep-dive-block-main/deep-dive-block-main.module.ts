9ad8a92cf93472b80c5717aa25f8509b2bfd0f4a
victoriale <le.victoriavan@gmail.com>
2016-10-07 09:32:17 -0500

    deep dive block main modify

--- modules/deep-dive-blocks/deep-dive-block-main/deep-dive-block-main.module.ts
+++ modules/deep-dive-blocks/deep-dive-block-main/deep-dive-block-main.module.ts
@@ -10,31 +10,17 @@ declare var moment;
 })
 
 export class DeepDiveBlockMain implements OnInit {
-  newsStackTop: Array<ArticleStackData>;
-  newsStackRow: Array<ArticleStackData>;
-  recDataSports: Array<ArticleStackData>;
-  businessStackTop: Array<ArticleStackData>;
-  businessStackRow: Array<ArticleStackData>;
-  politicsStackTop: Array<ArticleStackData>;
-  politicsStackBatch: Array<ArticleStackData>;
-  recDataEntertain: Array<ArticleStackData>;
-  foodStackTop: Array<ArticleStackData>;
-  foodStackRow: Array<ArticleStackData>;
-  recDataHealth: Array<ArticleStackData>;
-  lifestyleStackTop: Array<ArticleStackData>;
-  lifestyleStackRow: Array<ArticleStackData>;
-  estateStackTop: Array<ArticleStackData>;
-  estateStackBatch: Array<ArticleStackData>;
-  recDataTravel: Array<ArticleStackData>;
-  weatherStackTop: Array<ArticleStackData>;
-  weatherStackRow: Array<ArticleStackData>;
-  recDataAuto: Array<ArticleStackData>;
-  videoDataTop1: Array<VideoStackData>;
-  videoDataBatch1: Array<VideoStackData>;
-  videoDataTop2: Array<VideoStackData>;
-  videoDataBatch2: Array<VideoStackData>;
-  videoDataTop3: Array<VideoStackData>;
-  videoDataBatch3: Array<VideoStackData>;
+  stackTop1: Array<ArticleStackData>;
+  stackRow1: Array<ArticleStackData>;
+  stackTop2: Array<ArticleStackData>;
+  stackRow2: Array<ArticleStackData>;
+  stackTop3: Array<ArticleStackData>;
+  stackRow3: Array<ArticleStackData>;
+  recData1: Array<ArticleStackData>;
+  recData2: Array<ArticleStackData>;
+
+  videoDataTopMain: Array<VideoStackData>;
+  videoDataBatchMain: Array<VideoStackData>;
 
   sectionNameNews: SectionNameData =  {
      icon: "fa-clock",
@@ -60,34 +46,6 @@ export class DeepDiveBlockMain implements OnInit {
      icon: "fa-film",
      title: "Entertainment"
    }
-  sectionNameFood: SectionNameData =  {
-     icon: "fa-cutlery",
-     title: "Food"
-   }
-  sectionNameHealth: SectionNameData =  {
-     icon: "fa-heartbeat",
-     title: "Health"
-   }
-  sectionNameLifestyle: SectionNameData =  {
-     icon: "fa-diamond",
-     title: "Lifestyle"
-   }
-  sectionNameEstate: SectionNameData =  {
-     icon: "fa-home-1",
-     title: "Real Estate"
-   }
-  sectionNameTravel: SectionNameData =  {
-     icon: "fa-plane",
-     title: "Travel"
-   }
-  sectionNameWeather: SectionNameData =  {
-     icon: "fa-cloud-1",
-     title: "Weather"
-   }
-  sectionNameAuto: SectionNameData =  {
-     icon: "fa-auto-shape",
-     title: "Automotive"
-   }
 
   geoLocation: string = "ks";//TODO
   articleCallLimit:number = 50;
@@ -100,9 +58,9 @@ export class DeepDiveBlockMain implements OnInit {
           let article4 = data.data.splice(1,4);
           let article6 = data.data.splice(1,6);
           let article8 = data.data.splice(1,8);
-          this.newsStackTop = this._deepDiveData.transformToArticleStack(article1, "Breaking News");
-          this.newsStackRow  = this._deepDiveData.transformToArticleStack(article8, "Breaking News");
-          this.recDataSports = this._deepDiveData.transformToArticleStack(article6, "Sports");
+          this.stackTop1 = this._deepDiveData.transformToArticleStack(article1, "Breaking News");
+          this.stackRow1  = this._deepDiveData.transformToArticleStack(article8, "Breaking News");
+          this.recData1 = this._deepDiveData.transformToArticleStack(article6, "Sports");
         },
         err => {
             console.log("Error getting Breaking News data");
@@ -113,12 +71,8 @@ export class DeepDiveBlockMain implements OnInit {
         data => {
           let videoOne = [data.data[0]];
           let videoBatch = data.data.splice(1,5);
-          this.videoDataTop1 = this._deepDiveData.transformDeepDiveVideoBatchData(videoOne);
-          this.videoDataBatch1 = this._deepDiveData.transformDeepDiveVideoBatchData(videoBatch);
-          this.videoDataTop2 = this._deepDiveData.transformDeepDiveVideoBatchData(videoOne);
-          this.videoDataBatch2 = this._deepDiveData.transformDeepDiveVideoBatchData(videoBatch);
-          this.videoDataTop3 = this._deepDiveData.transformDeepDiveVideoBatchData(videoOne);
-          this.videoDataBatch3 = this._deepDiveData.transformDeepDiveVideoBatchData(videoBatch);
+          this.videoDataTopMain = this._deepDiveData.transformDeepDiveVideoBatchData(videoOne);
+          this.videoDataBatchMain = this._deepDiveData.transformDeepDiveVideoBatchData(videoBatch);
         },
         err => {
           console.log("Error getting video batch data");
