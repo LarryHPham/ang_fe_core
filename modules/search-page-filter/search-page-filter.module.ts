11232883c4290ff57297f87d6febba8b7d1e18bc
Navyaeetaram <navya.eetaram@sntmedia.com>
2016-10-10 10:25:04 -0500

    add Dropdowns to search Filter

--- modules/search-page-filter/search-page-filter.module.ts
+++ modules/search-page-filter/search-page-filter.module.ts
@@ -4,5 +4,26 @@ import {Component} from "@angular/core";
     templateUrl:'./app/fe-core/modules/search-page-filter/search-page-filter.module.html'
 })
 export class SearchPageFilter{
-
+keywords=[{
+    key:"Business",
+    value:"Business",
+},
+    {
+        key:'Sports',
+        value:"Sports",
+    }];
+    Sorting=[{
+        key:"MostRecent",
+        value:"Most Recent",
+    },
+        {
+            key:'Older',
+            value:"Older",
+        }];
+    keywordClick(e){
+        //console.log(e);
+    }
+    sortingClick(e){
+        //console.log(e);
+    }
 }
