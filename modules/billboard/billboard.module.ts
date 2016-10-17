71800419d12882f842a816c27ca80a9cb4b6ac1a
lynchtroop5 <christopher.lynch@sntmedia.com>
2016-10-17 15:17:00 -0500

    Addition of billboard module: https://www.wrike.com/open.htm?id=110654544

---
+++ modules/billboard/billboard.module.ts
@@ -0,0 +1,12 @@
+import {Component, Input} from '@angular/core';
+import {Http, Headers} from '@angular/http';
+declare var jQuery:any;
+
+@Component({
+    selector: 'billboard-module',
+    templateUrl: './app/fe-core/modules/billboard/billboard.module.html',
+})
+
+export class BillboardModule {
+
+}
