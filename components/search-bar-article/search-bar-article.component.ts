23d51d2614bff2462bc21ef59231d21f85bb3f31
Navyaeetaram <navya.eetaram@sntmedia.com>
2016-10-18 11:27:36 -0500

    search changes

--- components/search-bar-article/search-bar-article.component.ts
+++ components/search-bar-article/search-bar-article.component.ts
@@ -8,8 +8,6 @@ import {FormGroup} from "@angular/forms";
 export class ArticleSearchBar{
     @Input() searchTitle:string;
     @Input() searchSubTitle:string;
-    @Input() subCategoryList:Array<any>;
-    @Input() isSearchModule;
     @Input() scope;
     searchBoxBackground: string = 'http://www.wallpapersxl.com/wallpapers/1366x768/dark-green/149216/dark-green-free-hd-leaves-field-windows-149216.jpg';
     public searchInput:FormGroup;
