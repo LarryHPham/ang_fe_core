70eeba32f80e4c3c81abc2b6e311c71ad4d5e40f
victoriale <le.victoriavan@gmail.com>
2016-11-16 15:12:39 -0600

    side kick and carousel deep dive module

---
+++ components/sidekick-wrapper/sidekick-wrapper.component.ts
@@ -0,0 +1,8 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'sidekick-wrapper',
+    templateUrl: './app/fe-core/components/sidekick-wrapper/sidekick-wrapper.component.html'
+})
+
+export class SidekickWrapper {}
