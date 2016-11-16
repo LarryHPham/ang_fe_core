66abcd4145cc58db9c2ea340227b60c302e4c37f
coreywynn <Corey.Wynn@sntmedia.com>
2016-11-16 14:48:41 -0600

    corey/update_angular2 - Updates to profile header and video module for profile pages

---
+++ components/sidekick-wrapper/sidekick-wrapper.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'sidekick-wrapper',
+    templateUrl: './app/fe-core/components/sidekick-wrapper/sidekick-wrapper.component.html'
+})
+
+export class SidekickWrapper {}
--- components/video-stack/video-stack.component.ts
+++ components/video-stack/video-stack.component.ts
@@ -1,13 +1,28 @@
 import { Component, Input } from '@angular/core';
+import { GlobalFunctions } from "../../../global/global-functions";
+
+//interfaces
 import { VideoStackData } from "../../interfaces/deep-dive.data";
+
+//pipes
+import { SanitizeHtml } from "../../pipes/safe.pipe";
 
 declare var moment;
 
 @Component({
   selector: 'video-stack-component',
-  templateUrl: './app/fe-core/components/video-stack/video-stack.component.html',
+  templateUrl: './app/fe-core/components/video-stack/video-stack.component.html'
 })
 
-export class VideoStackComponent{
+export class VideoStackComponent {
+  public articleData: any;
+
   @Input() videoData: Array<VideoStackData>;
+  @Input() state: any;
+  @Input() page: number;
+  @Input() isProfilePage:boolean;
+
+  formatDate(date, isProfilePage) {
+    return isProfilePage ? GlobalFunctions.sntGlobalDateFormatting(date,"timeZone") : GlobalFunctions.sntGlobalDateFormatting(date,"dayOfWeek");
+  }
 }
