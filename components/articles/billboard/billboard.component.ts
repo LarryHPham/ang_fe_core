1f77bb664b7f434dfbd5b25151e5e5a8100648e9
lynchtroop5 <christopher.lynch@sntmedia.com>
2016-11-18 09:17:23 -0600

    Fixes to get article page to show with new upgrade

--- components/articles/billboard/billboard.component.ts
+++ components/articles/billboard/billboard.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit, Input} from '@angular/core';
-import {SafeResourceUrl} from '@angular/platform-browser';
-//import {DomSanitizationService, SafeResourceUrl} from '@angular/platform-browser';
+import {SafeResourceUrl, DomSanitizer} from '@angular/platform-browser';
 import {GlobalSettings} from '../../../../global/global-settings';
 @Component({
     selector: 'billboard-component',
@@ -13,8 +12,8 @@ export class BillboardComponent implements OnInit{
     @Input() partnerId;
     @Input() scope;
     @Input() teamId;
-    //constructor(private _sanitizer:DomSanitizationService) {
-    //}
+    constructor(private _sanitizer:DomSanitizer) {
+    }
 
     ngOnInit() {
       let tdlScope = this.scope == 'nfl' ? "nfl": 'ncaaf';
@@ -23,6 +22,6 @@ export class BillboardComponent implements OnInit{
         } else {
             this.dangerousBillBoardUrl = GlobalSettings.widgetUrl()+"/widgets/sports/tdl_billboard.html?%7B%22team%22%3A%22" + this.teamId + "%22%2C%22remn%22%3A%22false%22%2C%22dom%22%3A%22" + this.partnerId + "%22%2C%22scope%22%3A%22" + tdlScope + "%22%7D";
         }
-        //this.safeBillBoardUrl = this._sanitizer.bypassSecurityTrustResourceUrl(this.dangerousBillBoardUrl);
+        this.safeBillBoardUrl = this._sanitizer.bypassSecurityTrustResourceUrl(this.dangerousBillBoardUrl);
     }
 }
