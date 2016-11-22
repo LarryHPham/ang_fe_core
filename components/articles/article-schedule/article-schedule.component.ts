584da3aed5c9bbdd1fd62df44c1a27dad91fe810
coreywynn <Corey.Wynn@sntmedia.com>
2016-11-22 10:35:23 -0600

    corey/update_angular2 - Added team page and daily-udpate module

--- components/articles/article-schedule/article-schedule.component.ts
+++ components/articles/article-schedule/article-schedule.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit } from "@angular/core";
-// import {Gradient} from '../../../../global/global-gradient';
+import {Gradient} from '../../../../global/global-gradient';
 
 @Component({
     selector: 'article-schedule-component',
@@ -20,18 +20,17 @@ export class ArticleScheduleComponent implements OnInit {
     }
 
     ngOnChanges() {
-        if (typeof this.homeData != 'undefined' && typeof this.awayData != 'undefined') {
-            this.awayHex = this.awayData[0].awayHex;
-            this.homeHex = this.homeData[0].homeHex;
-            // var fullGradient = Gradient.getGradientStyles([this.awayHex, this.homeHex], .75);
-            // if (fullGradient) {
-                // this.gradient = fullGradient;
-                this.gradient = 'default-gradient';
-            // }
-            // else {
-                this.defaultGradient = 'default-gradient';
-            // }
-            // this.getGradient(this.homeHex, this.awayHex);
-        }
-    }
+      if (typeof this.homeData != 'undefined' && typeof this.awayData != 'undefined') {
+          this.awayHex = this.awayData[0].awayHex;
+          this.homeHex = this.homeData[0].homeHex;
+          var fullGradient = Gradient.getGradientStyles([this.awayHex, this.homeHex], .75);
+          if (fullGradient) {
+              this.gradient = fullGradient;
+          }
+          else {
+              this.defaultGradient = 'default-gradient';
+          }
+          // this.getGradient(this.homeHex, this.awayHex);
+      }
+  }
 }
