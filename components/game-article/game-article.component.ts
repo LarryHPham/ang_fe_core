3848603472b4fb951f1d64a287a976815b8e5f3a
coreywynn <Corey.Wynn@sntmedia.com>
2016-12-12 16:10:10 -0600

    Updates to boxscores

--- components/game-article/game-article.component.ts
+++ components/game-article/game-article.component.ts
@@ -1,5 +1,24 @@
 import {Component, Input, OnInit} from '@angular/core';
 
+export interface gameAiImageConfig {
+  hoverText: string;
+  imageClass: string;
+  imageUrl: string;
+  urlRouteArray: string;
+}
+export interface gameAiArticle {
+  report: string;
+  date: string;
+  eventId: string;
+  eventType: any;
+  imageConfig: gameAiImageConfig;
+  keyword: string;
+  teaser: string;
+  title: string;
+  url: Array<any>;
+  urlRouteArray: string;
+}
+
 @Component({
     selector: 'game-article',
     templateUrl: './app/fe-core/components/game-article/game-article.component.html'
@@ -9,5 +28,7 @@ export class GameArticle implements OnInit{
     @Input() gameArticle:any;
     constructor() {}
 
-    ngOnInit(){}
+    ngOnInit(){
+      console.log('this.gameArticle - ',this.gameArticle);
+    }
 }
