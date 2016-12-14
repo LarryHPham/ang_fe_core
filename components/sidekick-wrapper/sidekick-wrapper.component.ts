619c3c73f0053ab2259fba18ca5eb833e5f6d766
victoriale <le.victoriavan@gmail.com>
2016-12-14 10:11:20 -0600

    update vertical widget inputs

--- components/sidekick-wrapper/sidekick-wrapper.component.ts
+++ components/sidekick-wrapper/sidekick-wrapper.component.ts
@@ -1,9 +1,11 @@
-
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
     selector: 'sidekick-wrapper',
     templateUrl: './app/fe-core/components/sidekick-wrapper/sidekick-wrapper.component.html'
 })
 
-export class SidekickWrapper {}
+export class SidekickWrapper {
+  @Input() category: string;
+  @Input() subCategory: string;
+}
