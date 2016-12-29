95ea3ee1ad78e9e4ee5425d3e40ce9fa5f5fb5ac
larryhpham <larrypham1991@yahoo.com>
2016-12-29 12:26:12 -0600

    COME BACK TO THIS not standard and duplicate component

--- components/disqus/disqus.component.ts
+++ components/disqus/disqus.component.ts
@@ -1,38 +1,43 @@
-import {Component, OnInit, Inject} from '@angular/core';
+import {Component, OnInit, Inject, Input} from '@angular/core';
 import {GlobalSettings} from "../../../global/global-settings";
-
-
+import { DOCUMENT } from '@angular/platform-browser';
+import { isBrowser } from 'angular2-universal';
 
 declare var DISQUS: any;
 
 @Component({
     selector: 'disqus-component',
     templateUrl: './app/fe-core/components/disqus/disqus.component.html',
-    inputs: ["comment"]
 })
 
 export class DisqusComponent implements OnInit{
-    ngOnInit(){
-        var script:any = document.createElement("script");
+    @Input() comment:any;
 
+    private _document: Document;
+    constructor(@Inject(DOCUMENT) private document: Document){ this._document = document }
+
+    ngOnInit(){
+      if(isBrowser){
+        var script:any = this._document.createElement("script");
         // DisQus Plugin
         script.innerHTML = (function(d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)){
-                DISQUS.reset({
-                    reload: true,
-                    config: function () {
-                        this.page.identifier = (window.location.pathname + " ").replace("/"," ");
-                        this.page.url = window.location.href + "#!newthread";
-                    }
-                });
-            }else{
-                js = d.createElement(s); js.id = id;
-                js.src = "//"+ GlobalSettings.getHomePageLinkName()+".disqus.com/embed.js";
-                fjs.parentNode.insertBefore(js, fjs);
-            }
-        }(document, 'script', 'disqusJS'));
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)){
+            DISQUS.reset({
+              reload: true,
+              config: function () {
+                this.page.identifier = (window.location.pathname + " ").replace("/"," ");
+                this.page.url = window.location.href + "#!newthread";
+              }
+            });
+          }else{
+            js = d.createElement(s); js.id = id;
+            js.src = "//"+ GlobalSettings.getHomePageLinkName()+".disqus.com/embed.js";
+            fjs.parentNode.insertBefore(js, fjs);
+          }
+        }(this._document, 'script', 'disqusJS'));
 
-        document.body.appendChild(script);
+        this._document.body.appendChild(script);
+      }
     }
 }
