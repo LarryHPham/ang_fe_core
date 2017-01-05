a5895fb9c74cab5051dd0111d8296c7f725b2872
coreywynn <Corey.Wynn@sntmedia.com>
2017-01-05 11:56:34 -0600

    added sidekick component to project

--- custom-directives/verticalWidgetScroll.directive.ts
+++ custom-directives/verticalWidgetScroll.directive.ts
@@ -10,6 +10,8 @@ export class verticalWidgetScrollDirective{
     }
     @HostListener('window:scroll',['$event']) onScroll(e){
         this.scrollWidget=e.target.getElementById('verticalSideScroller');
+        var partnerHeader= e.target.getElementById('topPartnerHeader');
+        var partnerHeaderHeight=partnerHeader? partnerHeader.offsetHeight : 0;
         var header = e.target.body.getElementsByClassName('header')[0];
         var sharebtns = e.target.getElementById('shareLinksBtn');
         var articleTitle = e.target.getElementById('articleMainTitle');
@@ -30,27 +32,24 @@ export class verticalWidgetScrollDirective{
 
         var scrollTop =e.target.documentElement.scrollTop?e.target.documentElement.scrollTop:e.target.body.scrollTop;
         let scrollUp = scrollTop - this.scrollTopPrev>0?true:false;
-        let scrolHeight=e.target.documentElement.scrollHeight?e.target.documentElement.scrollHeight:e.target.body.scrollHeight
+        let scrolHeight=e.target.documentElement.scrollHeight?e.target.documentElement.scrollHeight:e.target.body.scrollHeight;
         var scrollBottom = scrolHeight-scrollTop==e.target.body.clientHeight?true:false;
 
         this.scrollTopPrev=scrollTop;
         if(this.scrollWidget){
-            var scrollYaxis= window.pageYOffset? window.pageYOffset: window.scrollY;
+            var scrollYaxis= window.pageYOffset ? window.pageYOffset : window.scrollY;
             if(scrollYaxis>topCSS){
                if(scrollUp) {
                     var topstyle = scrollYaxis - topCSS + 'px';
                     this._render.setElementStyle(this.scrollWidget, 'top', topstyle);
                 }else{
-                    var headerTop=e.target.body.getElementsByClassName('header-top')[0];
-                    var partnerheadTop=e.target.getElementById('partner_header')?document.getElementById('partner_header').offsetHeight:0;
-                    var topstyle = headerTop.offsetHeight? scrollYaxis - topCSS + headerTop.offsetHeight + partnerheadTop + 35 + 'px' :scrollYaxis - topCSS + partnerheadTop + 'px';
+                    var topstyle = scrollYaxis - topCSS + partnerHeaderHeight + 35 +'px';
                     this._render.setElementStyle(this.scrollWidget, 'top', topstyle);
                 }
                 if(scrollBottom && window.innerHeight - footer.offsetHeight < 650){
-                    var newTopCSS =scrollYaxis - topCSS - bottomCSS + 'px';
+                    var newTopCSS =scrollYaxis - topCSS - bottomCSS  + partnerHeaderHeight+ 'px';
                     this._render.setElementStyle(this.scrollWidget,'top', newTopCSS);
                 }
-
 
             }else{
                 this._render.setElementStyle(this.scrollWidget, 'top', '0px');
