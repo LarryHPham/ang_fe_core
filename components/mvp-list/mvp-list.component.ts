7be83eab5fb77d7bf3272ad58e3b7e6a0f4015d8
coreywynn <Corey.Wynn@sntmedia.com>
2017-03-06 10:06:17 -0600

    Removing do/check from mvp-list...incomplete

--- components/mvp-list/mvp-list.component.ts
+++ components/mvp-list/mvp-list.component.ts
@@ -27,7 +27,8 @@ export class MVPListComponent implements DoCheck, OnInit  {
   @Output() tabSelectedListener = new EventEmitter();
   @Output() dropdownPositionSelection = new EventEmitter();
 
-  @Input() tabs: Array<MVPTabData>;
+  @Input() tabs;
+  @Input() tabData;
   @Input() carouselFooter: FooterStyle;
   @Input() selectedTabTitle: string;
   @Input() position: string;
@@ -45,30 +46,30 @@ export class MVPListComponent implements DoCheck, OnInit  {
   dropdownSelectedKey: string = 'cb';
 
   ngDoCheck() {
-    if ( this.tabs && this.tabs.length > 0 ) {
-      if ( !this.tabsLoaded  ) {
-        this.tabsLoaded = {};
-        if ( !this.selectedTabTitle ) {
-          this.selectedTabTitle = this.tabs[0].tabDisplayTitle;
-        }
-
-        this.tabSelected(this.selectedTabTitle);
-      }
-      else {
-        let selectedTab = this.getSelectedTab();
-
-        //Only run update carousel on first run
-        if ( selectedTab && selectedTab.listData && selectedTab.listData.length > 0 && !this.tabsLoaded[selectedTab.tabDisplayTitle]) {
-          if (this.carouselDataArray != null && this.carouselDataArray[0].description[0].textData[0].text != selectedTab.getCarouselData()[0].description[0].textData[0].text) {
-            this.updateCarousel(selectedTab);
-          }
-          else if (this.carouselDataArray == null) {
-            this.updateCarousel(selectedTab);
-          }
-          //this.tabsLoaded[selectedTab.tabDisplayTitle] = "qb";
-        }
-      }
-    }
+    // if ( this.tabs && this.tabs.length > 0 ) {
+    //   if ( !this.tabsLoaded  ) {
+    //     this.tabsLoaded = {};
+    //     if ( !this.selectedTabTitle ) {
+    //       this.selectedTabTitle = this.tabs[0].tabDisplayTitle;
+    //     }
+    //
+    //     this.tabSelected(this.selectedTabTitle);
+    //   }
+    //   else {
+    //     let selectedTab = this.getSelectedTab();
+    //
+    //     //Only run update carousel on first run
+    //     if ( selectedTab && selectedTab.listData && selectedTab.listData.length > 0 && !this.tabsLoaded[selectedTab.tabDisplayTitle]) {
+    //       if (this.carouselDataArray != null && this.carouselDataArray[0].description[0].textData[0].text != selectedTab.getCarouselData()[0].description[0].textData[0].text) {
+    //         this.updateCarousel(selectedTab);
+    //       }
+    //       else if (this.carouselDataArray == null) {
+    //         this.updateCarousel(selectedTab);
+    //       }
+    //       //this.tabsLoaded[selectedTab.tabDisplayTitle] = "qb";
+    //     }
+    //   }
+    // }
   } //ngDoCheck()
 
   constructor( private activateRoute: ActivatedRoute ) {
@@ -92,59 +93,74 @@ export class MVPListComponent implements DoCheck, OnInit  {
   }
 
   getSelectedTab() {
-    if ( !this.tabs ) return null;
-
-    var tabTitle = this.selectedTabTitle;
-    var matches = this.tabs.filter(tab => tab.tabDisplayTitle == tabTitle);
-
-    return matches.length > 0 ? matches[0] : null;
+    // if ( !this.tabs ) return null;
+    //
+    // var tabTitle = this.selectedTabTitle;
+    // var matches = this.tabs.filter(tab => tab.tabDisplayTitle == tabTitle);
+    //
+    // return matches.length > 0 ? matches[0] : null;
   } //getSelectedTab()
 
   //each time a tab is selected the carousel needs to change accordingly to the correct list being shown
   tabSelected(tabTitle){
-    this.selectedTabTitle = tabTitle;
-    var selectedTab = this.getSelectedTab();
-
-    if ( selectedTab ) {
-      this.detailedDataArray = null;
-      if(!selectedTab.listData){
-        selectedTab.isLoaded = false;
-        this.tabSelectedListener.next({
-          tab:selectedTab,
-          position:this.position
-        });
-      }
-      else {
-        this.tabSelectedListener.next({
-          tab: selectedTab,
-          position: this.position
-        });
-        this.updateCarousel(selectedTab);
-      }
-    }
+    // this.selectedTabTitle = tabTitle;
+    // var selectedTab = this.getSelectedTab();
+    //
+    // if ( selectedTab ) {
+    //   this.detailedDataArray = null;
+    //   if(!selectedTab.listData){
+    //     selectedTab.isLoaded = false;
+    //     this.tabSelectedListener.next({
+    //       tab:selectedTab,
+    //       position:this.position
+    //     });
+    //   }
+    //   else {
+    //     this.tabSelectedListener.next({
+    //       tab: selectedTab,
+    //       position: this.position
+    //     });
+    //     this.updateCarousel(selectedTab);
+    //   }
+    // }
   } //tabSelected
 
   updateCarousel(tab: MVPTabData) {
-    if ( tab.listData.length == 0 ) {
-      this.carouselDataArray = tab.getCarouselData()
-    }
-    else {
-      this.carouselDataArray = tab.getCarouselData();
-      this.detailedDataArray = tab.listData;
-    }
+      console.log('---updateCarousel---');
+      console.log('tab - ',tab);
+    // if ( tab.listData.length == 0 ) {
+    //   this.carouselDataArray = tab.getCarouselData()
+    // }
+    // else {
+    //   this.carouselDataArray = tab.getCarouselData();
+    //   this.detailedDataArray = tab.listData;
+    // }
+    this.carouselDataArray = tab.getCarouselData();
+    this.detailedDataArray = tab.listData;
 
     this.dropdownSelectedKey = this.position == null ? this.dropdownSelectedKey  : this.position;
   } //updateCarousel
 
-  ngOnChanges() {
-    // reset dropdown when new datapoints have populated
-    var matches = this.tabs.filter(tab => tab.tabDisplayTitle == this.selectedTabTitle);
-    if(matches.length > 0){
-      this.selectedTabTitle = matches[0].tabDisplayTitle;
-    }else{
-      this.selectedTabTitle = this.tabs[0].tabDisplayTitle;
-    }
-    this.dropDownFirstRun = true;
+  ngOnChanges(event) {
+      console.log('---ngOnChanges---');
+      console.log('event - ',event);
+      console.log('event.tabs.currentValue - ',event.tabs.currentValue);
+      console.log('tabData - ',this.tabData);
+    //   if (this.tabs) {
+    //       var matchingTab = this.tabs.filter( tab => tab.tabDisplayTitle == this.selectedTabTitle );
+    //       console.log('matchingTab[0] - ',matchingTab[0]);
+    //       console.log('matchingTab[0].listData - ',matchingTab[0].listData);
+    //     //   this.updateCarousel(matchingTab[0]);
+    //   }
+
+    // // reset dropdown when new datapoints have populated
+    // var matches = this.tabs.filter(tab => tab.tabDisplayTitle == this.selectedTabTitle);
+    // if(matches.length > 0){
+    //   this.selectedTabTitle = matches[0].tabDisplayTitle;
+    // }else{
+    //   this.selectedTabTitle = this.tabs[0].tabDisplayTitle;
+    // }
+    // this.dropDownFirstRun = true;
   }
 
   dropdownChanged($event) {
