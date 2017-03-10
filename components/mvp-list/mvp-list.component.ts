14a7ea0af8400a956a1b81b08d44b82c53743d8c
coreywynn <Corey.Wynn@sntmedia.com>
2017-03-10 10:58:19 -0600

    Updated MVP mod & component...removed ngDoChecks

--- components/mvp-list/mvp-list.component.ts
+++ components/mvp-list/mvp-list.component.ts
@@ -1,4 +1,4 @@
-import { Injectable, Component, OnInit, DoCheck, Output, EventEmitter, Input } from '@angular/core';
+import { Injectable, Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 //interfaces
@@ -23,54 +23,25 @@ export interface MVPTabData {
     templateUrl: './mvp-list.component.html'
 })
 
-export class MVPListComponent implements DoCheck, OnInit  {
+export class MVPListComponent implements OnInit {
   @Output() tabSelectedListener = new EventEmitter();
   @Output() dropdownPositionSelection = new EventEmitter();
 
-  @Input() tabs;
   @Input() tabData;
-  @Input() carouselFooter: FooterStyle;
+  @Input() tabs;
   @Input() selectedTabTitle: string;
+  @Input() selectedFilterTitle: string;
   @Input() position: string;
   @Input() sortOptions: Array<any>;
-  @Input() selectedKey;
 
   detailedDataArray: DetailListInput[]; //variable that is just a list of the detailed DataArray
   carouselDataArray: Array<any>;
   dropDownFirstRun: boolean = true;
   tabsLoaded: {[index:number]:string};
-
   listType: string;
   displayTab: string;
-
   dropdownSelectedKey: string = 'cb';
 
-  ngDoCheck() {
-    // if ( this.tabs && this.tabs.length > 0 ) {
-    //   if ( !this.tabsLoaded  ) {
-    //     this.tabsLoaded = {};
-    //     if ( !this.selectedTabTitle ) {
-    //       this.selectedTabTitle = this.tabs[0].tabDisplayTitle;
-    //     }
-    //
-    //     this.tabSelected(this.selectedTabTitle);
-    //   }
-    //   else {
-    //     let selectedTab = this.getSelectedTab();
-    //
-    //     //Only run update carousel on first run
-    //     if ( selectedTab && selectedTab.listData && selectedTab.listData.length > 0 && !this.tabsLoaded[selectedTab.tabDisplayTitle]) {
-    //       if (this.carouselDataArray != null && this.carouselDataArray[0].description[0].textData[0].text != selectedTab.getCarouselData()[0].description[0].textData[0].text) {
-    //         this.updateCarousel(selectedTab);
-    //       }
-    //       else if (this.carouselDataArray == null) {
-    //         this.updateCarousel(selectedTab);
-    //       }
-    //       //this.tabsLoaded[selectedTab.tabDisplayTitle] = "qb";
-    //     }
-    //   }
-    // }
-  } //ngDoCheck()
 
   constructor( private activateRoute: ActivatedRoute ) {
     this.activateRoute.params.subscribe(
@@ -90,86 +61,51 @@ export class MVPListComponent implements DoCheck, OnInit  {
       else {
         this.position = this.listType;
       }
-  }
+  } //ngOnInit
 
-  getSelectedTab() {
-    // if ( !this.tabs ) return null;
-    //
-    // var tabTitle = this.selectedTabTitle;
-    // var matches = this.tabs.filter(tab => tab.tabDisplayTitle == tabTitle);
-    //
-    // return matches.length > 0 ? matches[0] : null;
-  } //getSelectedTab()
 
-  //each time a tab is selected the carousel needs to change accordingly to the correct list being shown
-  tabSelected(tabTitle){
-    // this.selectedTabTitle = tabTitle;
-    // var selectedTab = this.getSelectedTab();
-    //
-    // if ( selectedTab ) {
-    //   this.detailedDataArray = null;
-    //   if(!selectedTab.listData){
-    //     selectedTab.isLoaded = false;
-    //     this.tabSelectedListener.next({
-    //       tab:selectedTab,
-    //       position:this.position
-    //     });
-    //   }
-    //   else {
-    //     this.tabSelectedListener.next({
-    //       tab: selectedTab,
-    //       position: this.position
-    //     });
-    //     this.updateCarousel(selectedTab);
-    //   }
-    // }
-  } //tabSelected
 
-  updateCarousel(tab: MVPTabData) {
-      console.log('---updateCarousel---');
-      console.log('tab - ',tab);
-    // if ( tab.listData.length == 0 ) {
-    //   this.carouselDataArray = tab.getCarouselData()
-    // }
-    // else {
-    //   this.carouselDataArray = tab.getCarouselData();
-    //   this.detailedDataArray = tab.listData;
-    // }
-    this.carouselDataArray = tab.getCarouselData();
-    this.detailedDataArray = tab.listData;
+    ngOnChanges(event) {
+        if ( this.tabData ) {
+            this.detailedDataArray = this.tabData ? this.tabData.listData : null;
+            this.updateCarousel(this.tabData);
+        }
+    } //ngOnChanges
 
-    this.dropdownSelectedKey = this.position == null ? this.dropdownSelectedKey  : this.position;
-  } //updateCarousel
 
-  ngOnChanges(event) {
-      console.log('---ngOnChanges---');
-      console.log('event - ',event);
-      console.log('event.tabs.currentValue - ',event.tabs.currentValue);
-      console.log('tabData - ',this.tabData);
-    //   if (this.tabs) {
-    //       var matchingTab = this.tabs.filter( tab => tab.tabDisplayTitle == this.selectedTabTitle );
-    //       console.log('matchingTab[0] - ',matchingTab[0]);
-    //       console.log('matchingTab[0].listData - ',matchingTab[0].listData);
-    //     //   this.updateCarousel(matchingTab[0]);
-    //   }
 
-    // // reset dropdown when new datapoints have populated
-    // var matches = this.tabs.filter(tab => tab.tabDisplayTitle == this.selectedTabTitle);
-    // if(matches.length > 0){
-    //   this.selectedTabTitle = matches[0].tabDisplayTitle;
-    // }else{
-    //   this.selectedTabTitle = this.tabs[0].tabDisplayTitle;
-    // }
-    // this.dropDownFirstRun = true;
-  }
+    getSelectedTab(tabTitle) {
+        if ( !this.tabs ) return null;
+        var matchingTab = this.tabs.filter(tab => tab.tabDisplayTitle == tabTitle);
+        return matchingTab.length > 0 ? matchingTab[0] : null;
+    } //getSelectedTab()
 
-  dropdownChanged($event) {
-    this.dropDownFirstRun = false;
-    this.position = $event;
-    this.dropdownPositionSelection.next({
-      tab: this.getSelectedTab(),
-      position: $event //position 'key' value
-    });
-    this.dropdownSelectedKey = this.position == null ? this.dropdownSelectedKey  : this.position;
-  }
+
+
+    //each time a tab is selected the carousel needs to change accordingly to the correct list being shown
+    tabSelected(tabTitle){
+        var selectedTab = this.getSelectedTab(tabTitle);
+        this.tabSelectedListener.next({
+            tabDataKey: selectedTab.tabDataKey,
+            position:this.position
+        });
+    } //tabSelected
+
+
+
+    dropdownChanged($event) {
+        this.dropdownSelectedKey = $event;
+        var selectedTab = this.getSelectedTab(this.selectedTabTitle);
+        this.dropdownPositionSelection.next({
+            tab: selectedTab.tabDataKey,
+            position: $event //position 'key' value
+        });
+    } //dropdownChanged
+
+
+
+    updateCarousel(tab: MVPTabData) {
+        this.carouselDataArray = tab.getCarouselData();
+        this.detailedDataArray = tab.listData;
+    } //updateCarousel
 }
--- modules/mvp/mvp.module.ts
+++ modules/mvp/mvp.module.ts
@@ -16,75 +16,49 @@ export class MVPModule implements OnChanges {
   @Output() tabSelectedListener = new EventEmitter();
   @Output() dropdownPositionSelection = new EventEmitter();
 
+  @Input() scope: string;
+  @Input() tabs: any;
   @Input() mvpData: Array<MVPTabData>;
-  @Input() title: string;
-  @Input() query: any;
-  @Input() filter1: any;
-  @Input() collegeDivisionAbbrv: string;
-  @Input() collegeDivisionFullAbbrv: string;
-  @Input() positionNameDisplay: string;
-  @Input() storedPartnerParam: string;
+  @Input() sortOptions: any;
+  @Input() selectedTabTitle: string;
+  @Input() selectedFilterTitle: string;
+  @Input() isError: boolean;
+  @Input() mvpModuleFooterParams: Array<any>;
 
   modHeadData: ModuleHeaderData;
   footerData: ModuleFooterData;
-  tabKey: string;
-  partnerIdParam: string;
 
-  public scope: string;
-
-  ngOnChanges() {
-    this.displayData(this.query.scope, this.query.position);
+  ngOnChanges(event) {
+      console.log('---ngOnChanges---');
+      console.log('event - ',event);
+      this.displayData();
   }
 
-  displayData(scope, position){
-    var type = this.query.statName.indexOf(position)>=0 ? position : "k";
-    var url;
-    this.partnerIdParam = this.storedPartnerParam ? '/'+this.storedPartnerParam : '/';
 
-    if ( scope == this.collegeDivisionAbbrv.toLowerCase() ) {
-      scope = this.collegeDivisionFullAbbrv;
-    }
-    if ( !position ) {
-      position = this.query.position;
-    }
 
+  tabSelected(tab) {
+      this.tabSelectedListener.next(tab);
+  }
+
+
+
+  dropdownChanged($event) {
+    this.dropdownPositionSelection.next($event);
+  }
+
+
+
+  displayData() {
     this.modHeadData = {
         moduleTitle: "Most Valuable Players",
-        moduleIdentifier: " - "+scope.toUpperCase() + " " + this.positionNameDisplay,
+        moduleIdentifier: " - "+this.scope.toUpperCase() + " " + this.selectedFilterTitle,
         hasIcon: false,
         iconClass: '',
     };
-
-    url = [
-      this.partnerIdParam,
-      this.query.scope,
-      'mvp-list',
-      this.query.position,
-      this.query.statName,
-      "1"
-    ];
-
     this.footerData = {
       infoDesc: 'Want to see everybody involved in this list?',
       text: 'VIEW THE LIST',
-      url: url
+      url: this.mvpModuleFooterParams
     };
-  }
-
-  tabSelected(tab) {
-    //OLD from TDL
-    //this.displayData(this.query.scope, VerticalGlobalFunctions.convertPositionAbbrv(tab.tab.tabDataKey) );
-
-    this.displayData(this.query.scope, tab.tab.tabDataKey);
-    this.tabKey = tab.tab.tabDataKey;
-    if (!tab.tab.listData) { //let the page handle the service call if there's no data
-      this.tabSelectedListener.next(tab);
-    }
-    else {}
-  }
-
-  dropdownChanged($event) {
-    this.displayData(this.query.scope, $event.position );
-    this.dropdownPositionSelection.next($event);
   }
 }
